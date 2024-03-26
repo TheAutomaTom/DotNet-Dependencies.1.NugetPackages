b46f48b035ff09e285ceaa201d523a10ca01993f
Tom Grossi <TheAutomaTom@gmail.com>
2024-03-25 20:23:13 -0500

    Init

---
+++ ConsoleApp/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+open System
+open ClassLibrary
+
+printfn "Zoltar says ''Make your wish!''"
+let yourWish = System.Console.ReadLine()
+
+printfn $"{DeepThinker.TheAnswer}";
+
