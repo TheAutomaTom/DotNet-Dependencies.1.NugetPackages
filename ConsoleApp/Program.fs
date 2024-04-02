f63d6faf761a9c184492476e1e170c20b8e9ca60
Tom Grossi <TheAutomaTom@gmail.com>
2024-04-02 16:00:47 -0500

    Work in progress

--- ConsoleApp/Program.fs
+++ ConsoleApp/Program.fs
@@ -1,9 +1,7 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-open System
+﻿open System
 open ClassLibrary
 
-printfn "Zoltar says ''Make your wish!''"
-let yourWish = System.Console.ReadLine()
+printfn "What is six times seven?"
+printfn $"{QuestionResolver.Answer}";
 
-printfn $"{DeepThinker.TheAnswer}";
-
+ignore (Console.ReadKey());
